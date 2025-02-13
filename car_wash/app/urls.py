8d93be5b2cbead673b8d22a71ab7c146ae6676ed
aslam <thoppilaslam0987654321@gmail.com>
2025-02-13 12:52:15 +0530

    new

--- car_wash/app/urls.py
+++ car_wash/app/urls.py
@@ -3,9 +3,12 @@ from . import views
 
 urlpatterns = [
     path('',views.shp_login, name='shp_login'),
+    path('shp_login1/', views.shp_login1, name='shp_login1'),
+    path('shp_logout',views.shp_logout),
 
     
     # --------------------------user--------------------
+    path('register/', views.register, name='register'),
     path('user_home/', views.user_home, name='user_home'),
     path('book_appointment/', views.book_appointment, name='book_appointment'),
 ]
--- car_wash/app/views.py
+++ car_wash/app/views.py
@@ -31,14 +31,63 @@ def shp_login(req):
     else:
         return render(req,'login.html')
 
+def shp_login1(req):
+    if 'car_wash' in req.session:
+        return redirect(shp_home)
+    if 'user' in req.session:
+        return redirect(user_home)
+    if req.method=='POST':
+        uname=req.POST['uname']
+        password=req.POST['pswd']
+        data=authenticate(username=uname,password=password)
+        if data:
+            if data.is_superuser:
+                login(req,data)
+                req.session['car_wash']=uname   #create session
+                return redirect(shp_home)
+            else:
+                login(req,data)
+                req.session['user']=uname   #create session
+                return redirect(user_home)
+        else:
+            messages.warning(req,'Invalid username or password.')
+            return redirect(shp_login)
+    
+    else:
+        return render(req,'login.html')
+
 
 def shp_home(request):
     return render(request, 'user/home.html')
+
+def shp_logout(req):
+    req.session.flush()          #delete session
+    logout(req)
+    return redirect(shp_login)
 
 
 
 
 # ---------------------------------user---------------------------------------------
+
+
+def register(req):
+    if req.method=='POST':
+        name=req.POST['name']
+        email=req.POST['email']
+        password=req.POST['password']
+        # send_mail('user registration','eshop account created', settings.EMAIL_HOST_USER, [email])
+        try:
+           
+            data=User.objects.create_user(first_name=name,email=email,password=password,username=email)
+            data.save()
+            return redirect(shp_login)
+        except:
+            messages.warning(req,'User already exists.')
+            return redirect(register)
+    else:
+        return render(req,'user/register.html')
+
 def user_home(request):
     return render(request, 'user/home.html')
 
